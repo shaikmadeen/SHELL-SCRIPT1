2a7382656a7ecce2d9d7d21ac32a31d1ce4dd12e
shaikmadeen <shaikmadeen29@gmail.com>
2024-04-22 18:56:50 +0530

    hello-world

--- 02-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=madeen
-PERSON2=gopi
-
-echo "$PERSON1: Hello $PERSON2, Good morning"
-echo "$PERSON2: Hi $PERSON1, very good morning"
-echo "$PERSON1: How are you $PERSON2"
-echo "$PERSON2: Iam fine $PERSON1 and what about you?"
