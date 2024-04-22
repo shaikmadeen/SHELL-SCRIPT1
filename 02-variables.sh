9c38b613f6c04bbddb698706efc18158168c6639
shaikmadeen <163907894+shaikmadeen@users.noreply.github.com>
2024-04-22 13:13:23 +0530

    variables added

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=madeen
+person2=gopi
+
+echo "$person1: Hello $person2, Good morning"
+echo "$person2: Hi $person1, very good morning"
+echo "$person1: How are you $person2"
+echo "$person2: Iam fine $person1 and what about you?"
